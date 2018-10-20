30194d076a5a5576849b5ac108292aed09cf585a
liangdalong <liangdalong2010@gmail.com>
2018-10-20 11:47:38 +0800

    测试

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
 # -*- coding: UTF-8 -*_
-import urllib
 import re
-
-
-def getHtmlContent(url):
-    page = urllib.urlopen(url)
-    return page.read()
-
-
-if __name__ == "__main__":
-    url = 'http://tieba.baidu.com/p/2256306796'
-    getHtmlContent(url)
