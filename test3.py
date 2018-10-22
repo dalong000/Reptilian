3b0efcb5769165d254278d25517b3eda9a4ab456
liangdalong <liangdalong2010@gmail.com>
2018-10-22 11:05:51 +0800

    增加多线程方法（异步）

---
+++ test3.py
@@ -0,0 +1,60 @@
+import re
+import requests
+import hashlib
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+p = ThreadPoolExecutor(30)#创建1个线程池
+
+
+def get_index(url):
+    respose = requests.get(url)
+    if respose.status_code == 200:
+        return respose.text
+
+def parse_index(res):
+    res = res.result()#进程执行完毕后，得到一个对象
+    urls = re.findall(r'class="items".*?href="(.*?)"',res,re.S)#把文本信息转换成1行匹配
+    for url in urls:
+        p.submit(get_detail(url))#获取详情页提交到线程池
+
+def get_detail(url):#只下载一个视频
+    if not url.startswith('http'):
+        url = 'http://www.xiaohuar.com%s' %url
+    result = requests.get(url)
+    if result.status_code == 200:
+        mp4_url_list = re.findall(r'id="media".*?src="(.*?)"',result.text,re.S)
+        if mp4_url_list:
+            mp4_url = mp4_url_list[0]
+            print(mp4_url)
+
+
+def save(url):
+    video = requests.get(url)
+    if video.status_code == 200:
+        m = hashlib.md5()
+        m.updata(url.encode('utf-8'))
+        m.updata(str(time.time()).encode('utf-8'))
+        filename = r'%s.mp4'% m.hexdigest()
+        filepath = r'D:\\%s'%filename
+        with open(filepath, 'wb') as f:
+            f.write(video.content)
+
+
+def main():
+    for i in range(5):
+        p.submit(get_index,'http://www.xiaohuar.com/list-3-%s.html'% i).add_done_callback(parse_index)
+        #1、先把爬主页的任务(get_index)异步提交到线程池
+        #2、get_index任务执行完成后，会通过回调函数add_done_callback()通知主线程，任务完成；
+        #3、把get_index执行结果（注意线程执行结果是对象，调用res=result()方法，才能获取真正的执行结果）
+        #当做参数传给parse_index
+        #4、parse_index任务执行完毕后。
+        #5、通过循环，再次把获取页面详情页 get_detall()任务提交到线程池执行
+
+
+if __name__ == '__main__':
+    main()
+    
+        
+    
+   
