4482be70c4aba5abdcc984a3be0356bb6920625a
liangdalong <liangdalong2010@gmail.com>
2018-10-22 09:42:06 +0800

    爬取代码

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
 # -*- coding: UTF-8 -*_
 import re
+import requests
+
+
+respose = requests.get('http://www.xiaohuar.com/v/')
+
+
+urls = re.findall(r'class="items".*?href="(.*?)"',respose.text,re.S)
+url = urls[5]
+result = requests.get(url)
+mp4_url = re.findall(r'id="media".*?src="(.*?)"',result.text,re.S)[0]
+
+video = requests.get(mp4_url)
+
+
+with open('D:\\a.mp4','wb') as f:
+    f.write(video.content)
